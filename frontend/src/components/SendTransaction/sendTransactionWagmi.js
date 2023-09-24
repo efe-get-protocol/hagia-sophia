bc44f7f75da867c93e1be68facf306efb749d5ea
efe-get-protocol <111286837+efe-get-protocol@users.noreply.github.com>
2023-09-24 05:49:44 +0200

    wagmi integration

---
+++ frontend/src/components/SendTransaction/sendTransactionWagmi.js
@@ -0,0 +1,102 @@
+import * as React from 'react'
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from 'wagmi'
+import SendIcon from '@mui/icons-material/Send';
+import { TextField, Button} from "@mui/material";
+
+const styles = {
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '700px', // Adjust the maximum width as needed
+    margin: '0 auto',
+    marginTop: '40px'
+  },
+  textField: {
+    marginBottom: '36px', // Adjust the spacing between fields as needed
+  },
+  submitButton: {
+    marginTop: '16px', // Adjust the spacing above the submit button as needed
+    backgroundColor: '#6A0DAD', // Byzantine Blue
+    color: 'white',
+  },
+};
+export function CreateResearchWagmi(props) {
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError,
+  } = usePrepareContractWrite({
+    address: '0x942380a100C0f489A163060f3a42359347FB4a2D',
+    abi: [
+      {
+        name: 'createResearch',
+        type: 'function',
+        stateMutability: 'nonpayable',
+        inputs: [
+            { "internalType": "string", "name": "title", "type": "string" },
+            { "internalType": "string", "name": "description", "type": "string" },
+            { "internalType": "string", "name": "documentUrl", "type": "string" },
+            { "internalType": "uint8", "name": "fundingType", "type": "uint8" },
+            { "internalType": "uint256", "name": "fundingLimit", "type": "uint256" },
+            {
+              "internalType": "uint256",
+              "name": "reviewFundingPercentage",
+              "type": "uint256"
+            },
+            {
+              "internalType": "address[]",
+              "name": "contributingResearchers",
+              "type": "address[]"
+            },
+            { "internalType": "uint256", "name": "reviewerLimit", "type": "uint256" },
+            {
+              "internalType": "uint256",
+              "name": "reviewDeadline",
+              "type": "uint256"
+            },
+            { "internalType": "string", "name": "image", "type": "string" }
+          ],
+        outputs: [],
+      },
+    ],
+    functionName: 'createResearch',
+    args:[props.title,
+      props.description,
+      props.documentUrl,
+      props.fundingType,
+      props.fundingLimit,
+      props.reviewFundingPercentage,
+      props.contributingResearchers,
+      props.reviewerLimit,
+      props.reviewDeadline,
+    props.image]
+  })
+  const { data, error, isError, write } = useContractWrite(config)
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  })
+
+  return (
+    <div className="research-form">
+      <div style={styles.form}>
+      <Button  type="submit"
+            variant="contained"
+            color="primary"
+            style={styles.submitButton}
+            
+            endIcon={<SendIcon />}
+            disabled={!write || isLoading} onClick={() => write()}>
+        {isLoading ? 'Submitting' : 'Submit'}
+      </Button>
+      {(isPrepareError || isError) && (
+        <div>Error: {(prepareError || error)?.message}</div>
+      )}
+    </div>
+    </div>
+  )
+}
--- frontend/src/pages/Application/ResearchForm.js
+++ frontend/src/pages/Application/ResearchForm.js
@@ -1,199 +1,205 @@
-
-
-import React, {useState} from "react";
-import { TextField, Button} from "@mui/material";
-import { Link } from "react-router-dom"
-import SendIcon from '@mui/icons-material/Send';
-import {ethers, BigNumber} from 'ethers';
+import React, { useState } from "react";
+import { TextField, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import SendIcon from "@mui/icons-material/Send";
 import { privateKey } from "../../env";
+// import {BigInt} from 'viem'
+import { CreateResearchWagmi } from "../../components/SendTransaction/sendTransactionWagmi";
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
 
-const INFURA_ID = 'e18cea1fcdc44e6a84e5ab03efd311af'
-const provider = new ethers.providers.JsonRpcProvider(`https://polygon-mainnet.g.alchemy.com/v2/Qni5uO72dYJS_s5J74W5ptgQmOVISuUS`)
+// const INFURA_ID = "e18cea1fcdc44e6a84e5ab03efd311af";
+// const provider = new ethers.providers.JsonRpcProvider(
+//   `https://polygon-mainnet.g.alchemy.com/v2/Qni5uO72dYJS_s5J74W5ptgQmOVISuUS`
+// );
 
-const publicKey = "0x30D38078D6117285d6730F971d3F50A9004a575B"
- 
-const contractAddress = "0x942380a100C0f489A163060f3a42359347FB4a2D";
-const wallet = new ethers.Wallet(privateKey, provider);
+// const publicKey = "0x30D38078D6117285d6730F971d3F50A9004a575B";
 
-const contractABI = ["function createResearch(string,string,string,uint8,uint256,uint256,address[],uint256,uint256,string) view returns ()"];
-const contract = new ethers.Contract(contractAddress, contractABI, wallet);
+// const contractAddress = "0x942380a100C0f489A163060f3a42359347FB4a2D";
+// const wallet = new ethers.Wallet(privateKey, provider);
+
+// const contractABI = [
+//   "function createResearch(string,string,string,uint8,uint256,uint256,address[],uint256,uint256,string) view returns ()",
+// ];
+// const contract = new ethers.Contract(contractAddress, contractABI, wallet);
 
 const styles = {
-    form: {
-      display: 'flex',
-      flexDirection: 'column',
-      maxWidth: '700px', // Adjust the maximum width as needed
-      margin: '0 auto',
-      marginTop: '40px'
-    },
-    textField: {
-      marginBottom: '36px', // Adjust the spacing between fields as needed
-    },
-    submitButton: {
-      marginTop: '16px', // Adjust the spacing above the submit button as needed
-      backgroundColor: '#6A0DAD', // Byzantine Blue
-      color: 'white',
-    },
-  };
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: "700px", // Adjust the maximum width as needed
+    margin: "0 auto",
+    marginTop: "40px",
+  },
+  textField: {
+    marginBottom: "36px", // Adjust the spacing between fields as needed
+  },
+  submitButton: {
+    marginTop: "16px", // Adjust the spacing above the submit button as needed
+    backgroundColor: "#6A0DAD", // Byzantine Blue
+    color: "white",
+  },
+};
 
 const ResearchForm = () => {
+  const { isConnected } = useAccount()
 
-    const [formData, setFormData] = useState({
-        title: '',
-        description: '',
-        documentUrl: '',
-        fundingLimit: '',
-        reviewerLimit: '',
-        reviewFundingPercentage: '',
-        image: '',
-      });
-    
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-      };
-      async function executeTransaction(approveTxUnsigned) {
-        approveTxUnsigned.chainId = 137;
-        approveTxUnsigned.nonce = await provider.getTransactionCount(publicKey);
-        approveTxUnsigned.gasPrice = await provider.getGasPrice()
-        approveTxUnsigned.gasLimit = 2000000
-        console.log("nonce", approveTxUnsigned.nonce)
-        const approveTxSigned = await wallet.signTransaction(approveTxUnsigned);
-        console.log("signed", approveTxSigned)
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    documentUrl: "",
+    fundingLimit: "",
+    reviewerLimit: "",
+    reviewFundingPercentage: "",
+    image: "",
+  });
 
-        const submittedTx = await provider.sendTransaction(approveTxSigned);
-        console.log("submitted", submittedTx)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  // async function executeTransaction(approveTxUnsigned) {
+  //   approveTxUnsigned.chainId = 137;
+  //   approveTxUnsigned.nonce = await provider.getTransactionCount(publicKey);
+  //   approveTxUnsigned.gasPrice = await provider.getGasPrice();
+  //   approveTxUnsigned.gasLimit = 2000000;
+  //   console.log("nonce", approveTxUnsigned.nonce);
+  //   const approveTxSigned = await wallet.signTransaction(approveTxUnsigned);
+  //   console.log("signed", approveTxSigned);
 
-        const approveReceipt = await submittedTx.wait();
-        console.log("receipet", approveReceipt)
-        return approveReceipt;
-      }
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        // Handle form submission here This is where etherjs
-        try {
-          console.log("hello")
-          const tx =  await contract.connect(wallet).populateTransaction.createResearch(formData.title, formData.description, formData.documentUrl, 0,  BigNumber.from(formData.fundingLimit), BigNumber.from(formData.reviewFundingPercentage), [],  BigNumber.from(formData.reviewerLimit),  BigNumber.from(0), "url");
-          console.log("tx", tx)
-          const ret = await executeTransaction(tx);
-          console.log(ret)
-        } catch (error) {
-          console.error("Error:", error);
-        }
+  //   const submittedTx = await provider.sendTransaction(approveTxSigned);
+  //   console.log("submitted", submittedTx);
 
-        
-      };
-      return (
-        <div className="research-form">
-          <h1>Please fill out the information below and wait for approval.</h1>
-        <form style={styles.form} onSubmit={(e) => handleSubmit(e)}>
-            <TextField
-                label="Title"
-                name="title"
-                fullWidth
-                required
-                onChange={handleChange}
-                value={formData.title}
-                style={styles.textField}
-                InputLabelProps={{
-                style: { color: 'white' },
-                }}
-                InputProps={{
-                style: { color: 'white', backgroundColor: '#6A0DAD' },
-                }}
-            />
-            <TextField
-                label="Description"
-                name="description"
-                fullWidth
-                required
-                onChange={handleChange}
-                value={formData.description}
-                style={styles.textField}
-                InputLabelProps={{
-                style: { color: 'white' },
-                }}
-                InputProps={{
-                style: { color: 'white', backgroundColor: '#6A0DAD' },
-                }}
-            />
-            <TextField
-                label="Research Document URL"
-                name="documentUrl"
-                fullWidth
-                required
-                onChange={handleChange}
-                value={formData.documentUrl}
-                style={styles.textField}
-                InputLabelProps={{
-                style: { color: 'white' },
-                }}
-                InputProps={{
-                style: { color: 'white', backgroundColor: '#6A0DAD' },
-                }}
-            />
-            <TextField
-                label="Funding Limit"
-                name="fundingLimit"
-                fullWidth
-                required
-                onChange={handleChange}
-                value={formData.fundingLimit}
-                style={styles.textField}
-                InputLabelProps={{
-                style: { color: 'white' },
-                }}
-                InputProps={{
-                style: { color: 'white', backgroundColor: '#6A0DAD' },
-                }}
-            />
-            <TextField
-                label="Reviewer Limit"
-                name="reviewerLimit"
-                fullWidth
-                required
-                onChange={handleChange}
-                value={formData.reviewerLimit}
-                style={styles.textField}
-                InputLabelProps={{
-                style: { color: 'white' },
-                }}
-                InputProps={{
-                style: { color: 'white', backgroundColor: '#6A0DAD' },
-                }}
-            />
-            <TextField
-                label="Review Funding Limit Percentage"
-                name="reviewFundingPercentage"
-                fullWidth
-                required
-                onChange={handleChange}
-                value={formData.reviewFundingPercentage}
-                style={styles.textField}
-                InputLabelProps={{
-                style: { color: 'white' },
-                }}
-                InputProps={{
-                style: { color: 'white', backgroundColor: '#6A0DAD' },
-                }}
-            />
+  //   const approveReceipt = await submittedTx.wait();
+  //   console.log("receipet", approveReceipt);
+  //   return approveReceipt;
+  // }
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   // Handle form submission here This is where etherjs
+  //   try {
+  //     console.log("hello");
+  //     // const tx =  await contract.connect(wallet).populateTransaction.createResearch(formData.title, formData.description, formData.documentUrl, 0,  BigNumber.from(formData.fundingLimit), BigNumber.from(formData.reviewFundingPercentage), [],  BigNumber.from(formData.reviewerLimit),  BigNumber.from(0), "url");
+  //     // console.log("tx", tx)
+  //     // const ret = await executeTransaction(tx);
 
-            <TextField
-                label="Organization Image"
-                name="image"
-                fullWidth
-                required
-                onChange={handleChange}
-                value={formData.image}
-                style={styles.textField}
-                InputLabelProps={{
-                style: { color: 'white' },
-                }}
-                InputProps={{
-                style: { color: 'white', backgroundColor: '#6A0DAD' },
-                }}
-            />
-          <Button
+  //     // console.log(ret)
+  //   } catch (error) {
+  //     console.error("Error:", error);
+  //   }
+  // };
+
+  console.log(isConnected)
+  return (
+    <div className="research-form">
+      <h1>Please fill out the information below and wait for approval.</h1>
+      <form style={styles.form}>
+        <TextField
+          label="Title"
+          name="title"
+          fullWidth
+          required
+          onChange={handleChange}
+          value={formData.title}
+          style={styles.textField}
+          InputLabelProps={{
+            style: { color: "white" },
+          }}
+          InputProps={{
+            style: { color: "white", backgroundColor: "#6A0DAD" },
+          }}
+        />
+        <TextField
+          label="Description"
+          name="description"
+          fullWidth
+          required
+          onChange={handleChange}
+          value={formData.description}
+          style={styles.textField}
+          InputLabelProps={{
+            style: { color: "white" },
+          }}
+          InputProps={{
+            style: { color: "white", backgroundColor: "#6A0DAD" },
+          }}
+        />
+        <TextField
+          label="Research Document URL"
+          name="documentUrl"
+          fullWidth
+          required
+          onChange={handleChange}
+          value={formData.documentUrl}
+          style={styles.textField}
+          InputLabelProps={{
+            style: { color: "white" },
+          }}
+          InputProps={{
+            style: { color: "white", backgroundColor: "#6A0DAD" },
+          }}
+        />
+        <TextField
+          label="Funding Limit"
+          name="fundingLimit"
+          fullWidth
+          required
+          onChange={handleChange}
+          value={formData.fundingLimit}
+          style={styles.textField}
+          InputLabelProps={{
+            style: { color: "white" },
+          }}
+          InputProps={{
+            style: { color: "white", backgroundColor: "#6A0DAD" },
+          }}
+        />
+        <TextField
+          label="Reviewer Limit"
+          name="reviewerLimit"
+          fullWidth
+          required
+          onChange={handleChange}
+          value={formData.reviewerLimit}
+          style={styles.textField}
+          InputLabelProps={{
+            style: { color: "white" },
+          }}
+          InputProps={{
+            style: { color: "white", backgroundColor: "#6A0DAD" },
+          }}
+        />
+        <TextField
+          label="Review Funding Limit Percentage"
+          name="reviewFundingPercentage"
+          fullWidth
+          required
+          onChange={handleChange}
+          value={formData.reviewFundingPercentage}
+          style={styles.textField}
+          InputLabelProps={{
+            style: { color: "white" },
+          }}
+          InputProps={{
+            style: { color: "white", backgroundColor: "#6A0DAD" },
+          }}
+        />
+
+        <TextField
+          label="Organization Image"
+          name="image"
+          fullWidth
+          required
+          onChange={handleChange}
+          value={formData.image}
+          style={styles.textField}
+          InputLabelProps={{
+            style: { color: "white" },
+          }}
+          InputProps={{
+            style: { color: "white", backgroundColor: "#6A0DAD" },
+          }}
+        />
+        {/* <Button
             type="submit"
             variant="contained"
             color="primary"
@@ -202,11 +208,24 @@ const ResearchForm = () => {
             endIcon={<SendIcon />}
           >
             Submit
-          </Button>
-        </form>
+          </Button> */}
+        {isConnected? <CreateResearchWagmi
+          title={formData.title}
+          description={formData.description}
+          documentUrl={formData.documentUrl}
+          fundingType={0}
+          fundingLimit={(formData.fundingLimit)}
+          reviewFundingPercentage={(
+            formData.reviewFundingPercentage
+          )}
+          contributingResearchers={[]}
+          reviewerLimit={(formData.reviewerLimit)}
+          reviewDeadline={(0)}
+          image={"url"}
+        /> : <Box>Connect your wallet</Box>}
+      </form>
     </div>
-    );
-      
-}
+  );
+};
 
-export default ResearchForm
+export default ResearchForm;

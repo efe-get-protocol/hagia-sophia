7943cef46fc24928c86271c602482345235424ce
efe-get-protocol <111286837+efe-get-protocol@users.noreply.github.com>
2023-09-24 02:30:41 +0200

    frontend styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,34 @@ import Review from './pages/Review/Review.js'
 import Research from './pages/Research/Research.js'
 import Login from './pages/Login/Login.js'
 import ResearchForm from './pages/Application/ResearchForm'
+import { ResearchProvider } from './providers/subgraph';
+import { useMemo } from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 
 function App() {
+  const subgraphClient = useMemo(() => {
+    const defaultOptions = {
+      query: {
+        fetchPolicy: 'network-only',
+      },
+      watchQuery: {
+        fetchPolicy: 'no-cache',
+      },
+    };
+
+    return new ApolloClient({
+      uri: "https://api.thegraph.com/subgraphs/name/efesozen7/hagia-sophia-2",
+      cache: new InMemoryCache(),
+      defaultOptions: defaultOptions,
+    });
+  }, ["https://api.thegraph.com/subgraphs/name/efesozen7/hagia-sophia-2"]);
   return (
     <div className="App" id="outer-container">
       <NavigationBar/>
+      <ApolloProvider client={subgraphClient}>
+
+      <ResearchProvider>
       <Routes>
         <Route path='/' element={ <Crowdfunding/> } />
         <Route path='/Crowdfunding' element={ <Crowdfunding/> } />
@@ -23,6 +45,9 @@ function App() {
         <Route path='/ResearchForm' element={<ResearchForm/>} />
         
       </Routes>
+      </ResearchProvider>
+      </ApolloProvider>
+
     </div>
   );
 }
---
+++ frontend/src/providers/subgraph.js
@@ -0,0 +1,138 @@
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
+
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const stateValues = {
+  contractIsLoading: true,
+  researches: [],
+  researchers: [],
+  bounties: [],
+  peerReviews: [], 
+};
+
+const contextValues = {
+  ...stateValues,
+  fetchSubgraphData: () => void 0,
+};
+
+export const ResearchContext = createContext(contextValues);
+
+export const ResearchProvider = ({ children }) => {
+
+  const [state, setState] = useState({
+  });
+
+  const polygonApolloClient = new ApolloClient({
+    uri: "https://api.thegraph.com/subgraphs/name/efesozen7/hagia-sophia-2",
+    cache: new InMemoryCache(),
+  });
+
+  const fetchResearches = useCallback(async () => {
+    try {
+        const result = await polygonApolloClient.query({
+            query: gql`
+              query {
+                researches(first: 10) {
+                  description
+                  documentUrl
+                  fundingLimit
+                  fundingReceived
+                  title
+                  image
+                }
+              }
+            `,
+          })
+
+      // Filter out memories with invalid JSON, and parse valid JSON
+      const researches = result.data.researches;
+
+      return researches;
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const fetchBounties= useCallback(async () => {
+    try {
+        const result = await polygonApolloClient.query({
+            query: gql`
+              query {
+                bounties(first: 10) {
+                  bountyAmount
+                  description
+                  documentUrl
+                  title
+                }
+              }
+            `,
+          })
+
+      const bounties = result.data.bounties;
+
+      return bounties;
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const fetchPeerReviews = useCallback(async () => {
+    try {
+        const result = await polygonApolloClient.query({
+            query: gql`
+              query {
+                peerReviews(first: 10) {
+                  feedback
+                  peerReviewId
+                  researchId
+                  researcherId
+                  documentUrl
+                  rating
+                }
+              }
+            `,
+          })
+
+      const peerReviews = result.data.peerReviews;
+
+      return peerReviews;
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const fetchSubgraphData = useCallback(async () => {
+    setState(() => {
+      return {
+        contractIsLoading: true,
+      };
+    });
+
+    const [bounties, peerReviews, researches] = await Promise.all([fetchBounties(), fetchPeerReviews(), fetchResearches()]);
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        contractIsLoading: false,
+        bounties: bounties,
+        peerReviews: peerReviews,
+        researches: researches,
+      };
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchSubgraphData();
+  }, []);
+
+  return (
+    <ResearchContext.Provider
+      value={{
+        ...state,
+        fetchSubgraphData,
+      }}
+    >
+      {children}
+    </ResearchContext.Provider>
+  );
+};

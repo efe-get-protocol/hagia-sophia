8999e43742c258fb8c6dc2d6e4189bd7da29c592
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-09-24 04:07:51 -0400

    profile page button id dialog

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -8,6 +8,16 @@ import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import TextField from '@mui/material/TextField'
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -21,6 +31,12 @@ const Item = styled(Paper)(({ theme }) => ({
 const Profile = () => {
   // Define user data (you can fetch this from an API or other source)
 
+  const [isClaimPrizeDialogOpen, setIsClaimPrizeDialogOpen] = useState(false);
+  const [isClaimFundDialogOpen, setIsClaimFundDialogOpen] = useState(false);
+  const [alertSuccess, setAlertSuccess] = useState(false);
+  const [researcherId, setResearcherId] = useState(''); // Initialize the state
+  const [peerRevId, setPeerRevId] = useState('')
+
   const userData = {
     name: 'John Doe',
     affiliation: 'University XYZ',
@@ -29,11 +45,24 @@ const Profile = () => {
     // Add more fields as needed
   };
 
+  const handleClaimPrizeClick = () => {
+    setIsClaimPrizeDialogOpen(true);
+  }
+  const handlePeerRevClick = () => {
+    setIsClaimFundDialogOpen(true);
+  }
+
   const handleClaimPrize = () => {
     //add logic
+
+    setIsClaimPrizeDialogOpen(false);
+    setAlertSuccess(true);
   }
   const handlePeerReviewPrize = () => {
     //add logic
+
+    setIsClaimFundDialogOpen(false);
+    setAlertSuccess(true);
   }
 
   return (
@@ -46,10 +75,70 @@ const Profile = () => {
           <Item sx={{ fontSize: '25px', fontWeight: 'bold' }}>Name: {userData.name}</Item>
           <Item sx={{ fontSize: '25px', fontWeight: 'bold' }}>Affiliation: {userData.affiliation}</Item>
           <Item sx={{ fontSize: '25px', fontWeight: 'bold' }}>Wallet Address: {userData.walletAddress}</Item>
-          <Button onClick={handleClaimPrize} variant="contained"  endIcon={<MonetizationOnIcon/>}>Cash in Funds</Button>
-          <Button onClick={handlePeerReviewPrize} variant="contained"  endIcon={<MonetizationOnIcon/>}>Collect Peer Review Funds</Button>
+          <Button onClick={handleClaimPrizeClick} variant="contained"  endIcon={<MonetizationOnIcon/>}>Collect Reward</Button>
+          <Button onClick={handlePeerRevClick} variant="contained"  endIcon={<MonetizationOnIcon/>}>Collect Peer Review Reward</Button>
         </Stack>
       </Box>
+
+      <Dialog open={isClaimPrizeDialogOpen} onClose={() => setIsClaimPrizeDialogOpen(false)}>
+      <DialogTitle>Collect Your Researcher Reward</DialogTitle>
+      <DialogContent>
+      <DialogContentText>
+          Please enter your Researcher ID
+      </DialogContentText>
+      <TextField
+            autoFocus
+            margin="dense"
+            id="researcherId"
+            label="Researcher ID"
+            type="string"
+            fullWidth
+            value={researcherId} // Set the value from state
+            onChange={(e) => setResearcherId(e.target.value)} // Update the state on change
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setIsClaimPrizeDialogOpen(false)} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleClaimPrize} color="primary">
+          Submit
+        </Button>
+      </DialogActions>
+      </Dialog>
+
+      <Snackbar open={alertSuccess} autoHideDuration={3000} onClose={() => setAlertSuccess(false)}>
+        <MuiAlert elevation={6} variant="filled" onClose={() => setAlertSuccess(false)} severity="success">
+          Successfully Claimed Prize!
+        </MuiAlert>
+      </Snackbar>
+
+      <Dialog open={isClaimFundDialogOpen} onClose={() => setIsClaimFundDialogOpen(false)}>
+      <DialogTitle>Collect Your Peer Review Reward</DialogTitle>
+      <DialogContent>
+      <DialogContentText>
+          Please enter your Peer Review ID
+      </DialogContentText>
+      <TextField
+            autoFocus
+            margin="dense"
+            id="peerRevId"
+            label="Peer Review ID"
+            type="string"
+            fullWidth
+            value={peerRevId} // Set the value from state
+            onChange={(e) => setPeerRevId(e.target.value)} // Update the state on change
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setIsClaimFundDialogOpen(false)} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handlePeerReviewPrize} color="primary">
+          Submit
+        </Button>
+      </DialogActions>
+      </Dialog>
 
   </div>
   );

abce0ee0ee5dc4e1d3c209e32594e325ab45b138
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2023-09-24 01:12:52 -0400

    changed bounties page card to have apply

---
+++ frontend/src/components/BountyAction/BountyAction.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+import {useState} from 'react'
+
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const BountyAction = ({image, organization, description}) => {
+
+    const [alertSuccess, setAlertSuccess] = useState(false);
+
+
+    const handleApply = () => {
+        // logic to apply to bounty
+        setAlertSuccess(true);
+    }
+
+  return (
+    <div>
+    <Card sx={{ maxWidth: 475 }}>
+      <CardMedia
+        sx={{ height: 440 }}
+        image={image}
+        title="research-card"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {organization}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+        <CardActions>
+        <Button onClick={handleApply} style={{color: 'white', margin: 'auto', backgroundColor: 'blue'}} size="small">Apply</Button>
+        </CardActions>
+    </Card>
+
+    <Snackbar open={alertSuccess} autoHideDuration={3000} onClose={() => setAlertSuccess(false)}>
+        <MuiAlert elevation={6} variant="filled" onClose={() => setAlertSuccess(false)} severity="success">
+          Successfully Applied!
+        </MuiAlert>
+      </Snackbar>
+    </div>
+
+
+    
+  );
+}
+
+export default BountyAction
--- frontend/src/pages/Bounties/Bounties.js
+++ frontend/src/pages/Bounties/Bounties.js
@@ -1,6 +1,6 @@
 import React, {useState, useContext} from "react";
 import './Bounties.css'
-import Infographic from '../../components/Infographic/Infographic'
+import BountyAction from "../../components/BountyAction/BountyAction";
 import Button from '@mui/material/Button'
 import SendIcon from '@mui/icons-material/Send';
 import { Link } from 'react-router-dom';
@@ -10,16 +10,17 @@ const Bounties = () => {
 
   const { bounties } = useContext(ResearchContext);
 
-
+  
 
 
   if(bounties) return (
     <div> 
+      <br/>
       <Button component={Link} to="/BountyForm" variant="contained" endIcon={<SendIcon />} >Create Bounty</Button>
       <br/>
       <br/>
       <div className="info-container">
-      {bounties.map(item => <Infographic image={item.image} organization={item.title} description={item.description} amount_raised={item.bountyAmount} target_amount={item.fundingLimit} />
+      {bounties.map(item => <BountyAction image={"science_background_image.jpeg"} organization={item.title} description={item.description} />
           )}
       </div>
     </div>
